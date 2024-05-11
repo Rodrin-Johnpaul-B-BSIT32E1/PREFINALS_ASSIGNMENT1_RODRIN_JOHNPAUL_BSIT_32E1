dcca1df5d34dc8348a1147ec3ce3cbcf3545da46
Bande_Meliza_BSIT32E1 <161306927+Rodrin-Johnpaul-B-BSIT32E1@users.noreply.github.com>
2024-05-11 14:49:59 +0800

    add all

--- AuthServer/obj/Debug/net8.0/AuthServer.AssemblyInfo.cs
+++ AuthServer/obj/Debug/net8.0/AuthServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a4d7a9c89d6cb90194b4f9a2acd8c1e85187288")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ ProtectedApi/Controllers/ValuesController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace ProtectedApi
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValueController : ControllerBase
+    {
+        private readonly string _owner = "John Paul Rodrin";
+        private readonly Random _random = new Random();
+        private readonly string[] _thingsAboutOwner = new[]
+        {
+            "I love playing ml.",
+            "I enjoy riding.",
+            "I am working student.",
+            "I like basketball.",
+            "I love sleeping.",
+            "I love eating.",
+            "I play on my phone when I have free time.",
+            "I love my girlfriend.",
+            "I love myself.",
+            "I enjoy traveling."
+        };
+
+        [HttpGet("about/me")]
+        public IActionResult AboutMe()
+        {
+            var thing = _thingsAboutOwner[_random.Next(_thingsAboutOwner.Length)];
+            return Ok(thing);
+        }
+
+        [HttpGet("about")]
+        public IActionResult About()
+        {
+            return Ok(_owner);
+        }
+
+        [HttpPost("about")]
+        public IActionResult About([FromBody] NameModel model)
+        {
+            return Ok($"Hi {model.Name} from {_owner}");
+        }
+    }
+
+    public class NameModel
+    {
+        public string? Name { get; set; }
+    }
+}
