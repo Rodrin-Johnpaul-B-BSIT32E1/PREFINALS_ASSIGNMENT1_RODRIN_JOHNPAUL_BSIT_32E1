b3217630eb748f4fab94b9eb92650ef45f1be633
Bande_Meliza_BSIT32E1 <161306927+Rodrin-Johnpaul-B-BSIT32E1@users.noreply.github.com>
2024-05-11 14:28:08 +0800

    update JwtSetting.cs

--- AuthServer/JwtSettings.cs
+++ AuthServer/JwtSettings.cs
@@ -2,5 +2,8 @@
 {
     public class JwtSettings
     {
+        public string? Issuer { get; set; }
+        public string? Audience { get; set; }
+        public int ExpiryInMinutes { get; set; }
     }
 }
